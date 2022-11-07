9e0e6e01e1e0e2f79ec783fa9416c9785b9ca7c2
Vinclin123 <116074139+Vinclin123@users.noreply.github.com>
2022-11-08 03:49:32 +0530

    website model

---
+++ login.php
@@ -0,0 +1,30 @@
+<?php
+include('config.php');
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "photography";
+session_start();
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+ ///function login($username,$password)
+    $username=$_POST["username"];
+    $password=$_POST["password"];
+    if($username!="" && $password!="")
+    {
+       $sql="SELECT username,password1 FROM registration WHERE username='$username' AND password1='$password'";
+       echo $sql;
+       $result = mysqli_query($conn, $sql);
+       $data = mysqli_fetch_assoc($result);
+       if($data['username']!="")
+       {
+        $_SESSION["username"]=$data['username'];
+        echo"<script>window.location='index.php'</script>";
+       }
+       else
+       {
+        echo "<script>window.location='index2.html'</script>";
+       }
+       
+    }
+?>
